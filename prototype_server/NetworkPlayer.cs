89cabaddfe523a3253f16449c4d4cd6135ebf35f
Hirad Yazdanpanah <hiradyazdan@users.noreply.github.com>
2019-03-06 17:28:26 +0000

    initial commit

---
+++ prototype_server/NetworkPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LiteNetLib;
+
+namespace PrototypeServer
+{
+    public class NetworkPlayer
+    {
+        public float x = 0.0f;
+        public float y = 0.0f;
+        public float z = 0.0f;
+
+        public NetPeer netPeer;
+        public bool moved = false;
+
+
+        public NetworkPlayer(NetPeer peer)
+        {
+            this.netPeer = peer;
+        }
+
+    }
+}
