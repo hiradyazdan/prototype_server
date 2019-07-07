6695841b649d6ad7643fc754fd27ceb508f9cd64
Hirad Yazdanpanah <hiradyazdan@users.noreply.github.com>
2019-07-07 22:22:28 +0100

    wip - adding benchmark functionality

---
+++ prototype_server.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prototype_server.Benchmark
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
